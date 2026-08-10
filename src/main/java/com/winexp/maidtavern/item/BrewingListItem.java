c5e434b7c2b20a434dafa643771220b89ebd3f03
WinExp <winexp2333@qq.com>
2026-08-10 21:14:18 +0800

    fix: crash in dedicated server

--- src/main/java/com/winexp/maidtavern/item/BrewingListItem.java
+++ src/main/java/com/winexp/maidtavern/item/BrewingListItem.java
@@ -1,12 +1,10 @@
 package com.winexp.maidtavern.item;
 
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
-import com.winexp.maidtavern.client.gui.brewing_list.BrewingListScreen;
+import com.winexp.maidtavern.util.ScreenUtil;
 import com.winexp.maidtavern.entity.MaidTavernEntities;
 import com.winexp.maidtavern.maid.brew.BrewingList;
 import com.winexp.maidtavern.maid.brew.IBrewTask;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -21,12 +19,12 @@ public class BrewingListItem extends Item implements MaidInteractionItem {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        ItemStack stack = player.getItemInHand(usedHand);
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
         if (player.isShiftKeyDown()) return InteractionResultHolder.pass(stack);
         if (level.isClientSide) {
             BrewingList brewingList = stack.get(MaidTavernItems.BREWING_LIST_DATA);
-            Minecraft.getInstance().setScreen(new BrewingListScreen((LocalPlayer) player, usedHand, brewingList));
+            ScreenUtil.openBrewingListScreen(player, hand, brewingList);
         }
         return InteractionResultHolder.sidedSuccess(stack, level.isClientSide);
     }
---
+++ src/main/java/com/winexp/maidtavern/util/ScreenUtil.java
@@ -0,0 +1,14 @@
+package com.winexp.maidtavern.util;
+
+import com.winexp.maidtavern.client.gui.brewing_list.BrewingListScreen;
+import com.winexp.maidtavern.maid.brew.BrewingList;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+
+public class ScreenUtil {
+    public static void openBrewingListScreen(Player player, InteractionHand hand, BrewingList brewingList) {
+        Minecraft.getInstance().setScreen(new BrewingListScreen((LocalPlayer) player, hand, brewingList));
+    }
+}
